24c65acf29872949573a28dd8e928652970fd801
nhd1123 <parkunduong@gmail.com>
2021-10-07 20:16:27 +0700

    add unit test

--- src/test/java/com/demo/employeemanagement/EmployeemanagementApplicationTests.java
+++ src/test/java/com/demo/employeemanagement/EmployeemanagementApplicationTests.java
@@ -1,13 +1,30 @@
 package com.demo.employeemanagement;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.List;
+
+import com.demo.employeemanagement.model.Employee;
+import com.demo.employeemanagement.repository.EmployeeRepository;
+import com.demo.employeemanagement.service.EmployeeService;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeemanagementApplicationTests {
+	@Autowired
+	EmployeeRepository employeeRepository;
+	@Autowired
+	EmployeeService employeeService;
 
 	@Test
-	void contextLoads() {
+	public void testApi() {
+		List<Employee> emp = employeeService.getEmployees();
+		// assertEquals(expected, actual);
+		assertFalse(emp.isEmpty());
+
 	}
 
 }
