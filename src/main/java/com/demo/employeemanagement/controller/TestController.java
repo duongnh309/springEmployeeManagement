4651072beb8bc7a647615fb36a7d13d3c75558f9
nhd1123 <parkunduong@gmail.com>
2021-10-07 14:55:06 +0700

    switch branch

---
+++ src/main/java/com/demo/employeemanagement/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.demo.employeemanagement.controller;
+
+import com.demo.employeemanagement.service.EmployeeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TestController {
+    @Autowired
+    EmployeeService employeeService;
+
+}
